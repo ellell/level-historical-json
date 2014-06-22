87ddd7a05fc41ffe808302d2a551863feefc52e6
Lisa Övermyr <lisaovermyr@gmail.com>
2014-06-22 16:33:24 +0200

    Here we go
     Basic save and get

---
+++ level-historical-json.js
@@ -0,0 +1,87 @@
+var endpoint = require('endpoint')
+  , Sublevel = require('level-sublevel')
+  , subStream = require('level-sublevel-stream')
+  , series = require('run-series')
+  , ObjectId = require('node-time-uuid')
+
+  , LHJ = function (db) {
+    if (!(this instanceof LHJ))
+      return new LHJ(db)
+
+    this.db = db;
+  }
+
+LHJ.prototype.put = function (obj, callback) {
+  var subKey = obj.key || new ObjectId()
+    , db = Sublevel(this.db).sublevel(subKey, { valueEncoding: 'json' })
+    , ops = [];
+
+  Object.keys(obj).forEach(function (key, i) {
+    var value = {}
+    value[key] = obj[key]
+
+    ops.push({
+      type: 'put'
+      , key: new ObjectId()
+      , value: value
+    })
+  })
+
+  db.batch(ops, callback)
+}
+
+LHJ.prototype._getSubKeys = function (query, callback) {
+  if (typeof query === 'function') {
+    callback = query
+    query = {}
+  }
+
+  subStream(this.db, query)
+    .pipe(endpoint({ objectMode: true }, function (err, subkeys) {
+      if (err) return callback(err)
+
+      callback(null, subkeys)
+    }))
+}
+
+LHJ.prototype.get = function (query, callback) {
+  var self = this
+    , tasks = []
+    , getLatestValues
+
+  if (typeof query === 'function') {
+    callback = query
+    query = {}
+  }
+
+  getLatestValues = function (subkey) {
+    return function (callback) {
+      var subdb = Sublevel(self.db).sublevel(subkey, { valueEncoding: 'json' })
+        , obj = {
+          key: subkey
+        }
+
+      subdb.createReadStream()
+        .pipe(endpoint({ objectMode: true }, function (err, objParts) {
+          if (err) return callback(err)
+
+          for (var i = 0;  objParts.length > i; i++) {
+            for (prop in objParts[i].value) {
+              obj[prop] = objParts[i].value[prop]
+            }
+          }
+
+          callback(null, obj)
+        }))
+    }
+  }
+
+  this._getSubKeys(query, function (err, subkeys) {
+    if (err) return callback(err)
+
+    tasks = subkeys.map(getLatestValues)
+    series(tasks, callback)
+  })
+}
+
+module.exports = LHJ
---
+++ test.js
@@ -0,0 +1,90 @@
+var test = require('tap').test
+  , db = require('level-test')()
+  , series = require('run-series')
+
+  , Service = require('./level-historical-json')
+
+
+function createObj() {
+  return {
+    title: 'title'
+    , description: 'description...'
+  }
+}
+
+test('should be able to save and get data', function (t) {
+  var service = Service(db())
+    , tasks = []
+
+  tasks.push(function (callback) {
+    service.put({beep: 'boop'}, callback)
+  })
+  tasks.push(function (callback) {
+    service.put({foo: 'bar'}, callback)
+  })
+  tasks.push(function (callback) {
+    service.put({hey: 'ahoy'}, callback)
+  })
+  tasks.push(function (callback) {
+    service.get(function (err, res) {
+      if (err) return callback(err)
+
+      t.equal(res.length, 3)
+      t.equal(res[0].beep, 'boop')
+      t.equal(res[1].foo, 'bar')
+      t.equal(res[2].hey, 'ahoy')
+      callback()
+    })
+  })
+
+  series(tasks, function (err) {
+    t.notOk(err)
+
+    t.end()
+  })
+})
+
+test('should be able to update data', function (t) {
+  var service = Service(db())
+    , tasks = []
+    , obj = {
+      beep: 'boop'
+      , foo: 'bar'
+      , hey: 'ahoy'
+    }
+
+  tasks.push(function (callback) {
+    service.put(obj, callback)
+  })
+
+  tasks.push(function (callback) {
+    service.get(function (err, res) {
+      if (err) return callback(err)
+
+      t.equal(res[0].beep, 'boop')
+      t.equal(res[0].foo, 'bar')
+      t.equal(res[0].hey, 'ahoy')
+
+      var newEvent = res[0]
+      service.put({ hey: 'updated', key: res[0].key }, callback)
+    })
+  })
+
+  tasks.push(function (callback) {
+    service.get(function (err, res) {
+      if (err) console.log('err', err)
+
+      t.equal(res[0].beep, 'boop')
+      t.equal(res[0].foo, 'bar')
+      t.equal(res[0].hey, 'updated')
+
+      callback()
+    })
+  })
+
+  series(tasks, function (err) {
+    t.notOk(err)
+
+    t.end()
+  })
+})
